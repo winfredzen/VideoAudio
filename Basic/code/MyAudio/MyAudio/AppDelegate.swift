1717e6a7c5dc59691119bca29e544fad6210eb5d
王振 <wangzh@szlanyou.com>
2022-06-22 16:42:22 +0800

    音频采集

---
+++ Basic/code/MyAudio/MyAudio/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyAudio
+//
+//  Created by 王振 on 2022/6/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Basic/code/MyAudio/MyAudio/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyAudio
+//
+//  Created by 王振 on 2022/6/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
